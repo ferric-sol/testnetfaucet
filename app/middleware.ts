a1c204444dcc4d9c53626e6576a712865285e055
ferric <ferric-sol@ferric.net>
2024-02-16 00:45:45 -0600

    Ghetto tracking

--- app/middleware.ts
+++ app/middleware.ts
@@ -16,6 +16,7 @@ export const config = {
 export default async function middleware(request: NextRequest) {
   // You could alternatively limit based on user ID or similar
   const ip = request.ip ?? '127.0.0.1';
+  await kv.incr(`ip_${ip}`);
   const { success, pending, limit, reset, remaining } = await ratelimit.limit(
     ip
   );
