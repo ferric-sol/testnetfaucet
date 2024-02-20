2ee3df1a669fddec759de5488437b014975d9bc8
ferric <ferric-sol@ferric.net>
2024-02-20 02:59:03 -0600

    Copy

--- middleware.ts
+++ middleware.ts
@@ -17,6 +17,7 @@ export default async function middleware(request: NextRequest) {
   // You could alternatively limit based on user ID or similar
   const ip = request.ip ?? '127.0.0.1';
   await kv.incr(`ip_${ip}`);
+  console.log(await kv.get(`ip_${ip}`))
   const { success, pending, limit, reset, remaining } = await ratelimit.limit(
     ip
   );
